cafc3239c8517fade835ba67b9c254ccb7afe6f1
Santiago <santiagoferrero2000@gmail.com>
2022-07-14 10:57:58 -0300

    creacion del menu de aplicaciones y el archivo texto de entrada. comienzo del ejercicio 1.

---
+++ menu_de_aplicaciones.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source ./statsWords.sh 
+texto=$(cat $@)
+op=1
+while [[ $op -ne 0 ]]; 
+do	
+	read -p 'Seleccione una opcion de 1 a 5, 0 para fin de datos' op
+	case $op in
+		[1]*) statswords $texto ;;
+		[2]*) echo opcion2 ;;
+		[0]*0) break ;;
+	esac
+done
+
