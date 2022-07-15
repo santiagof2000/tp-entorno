e6e7c972653536cdcb1064810467f3a9ebb8b0b7
Santiago <santiagoferrero2000@gmail.com>
2022-07-15 16:19:30 -0300

    ejercicio 1 completado

--- menu_de_aplicaciones.sh
+++ menu_de_aplicaciones.sh
@@ -5,7 +5,7 @@ texto=$(cat $@)
 op=1
 while [[ $op -ne 0 ]]; 
 do	
-	read -p 'Seleccione una opcion de 1 a 5, 0 para fin de datos' op
+	read -p 'Seleccione una opcion de 1 a 5, 0 para fin de datos: ' op
 	case $op in
 		[1]*) statswords $texto ;;
 		[2]*) echo opcion2 ;;
--- statsWords.sh
+++ statsWords.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
 function statswords(){
-	echo $@
-}
+#	cat $@ | sort | head --lines=1 | uniq > ordenadas.txt
+	PALABRAMAYOR=''
+	for PALABRA in $@
+        do
+	        if [ ${#PALABRA} -gt ${#PALABRAMAYOR} ]
+       		then
+                	PALABRAMAYOR=$PALABRA
+        	fi
+        done
+	echo 'Palabra mas larga: ' $PALABRAMAYOR
+
+	PALABRAMENOR='fdsaaadddffffffffffffgfdgdfgfdgdf'
+        for PALABRA in $@
+        do
+		if [ ${#PALABRA} -lt ${#PALABRAMENOR} ]
+                then
+                        PALABRAMENOR=$PALABRA
+                fi
+        done
+        echo 'Palabra mas corta: ' $PALABRAMENOR
+	
+	CONTADOR=0
+	ACUMULADOR=0
+	for PALABRA in $@
+	do
+		ACUMULADOR=$((ACUMULADOR+${#PALABRA}))
+		CONTADOR=$((CONTADOR+1))
+	done
+	echo 'Promedio de letras por palabra: ' $((ACUMULADOR/CONTADOR))
+	}
+
